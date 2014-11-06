78b4b94f3b3b4a04f5ec40f1a74c691829eee0d8
sonir <isana@sonir.jp>
2014-11-07 02:20:35 +0900

    Ver0.2 write serial

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,16 +4,78 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    
+    //OSC
     receiver.setup(PRINT_PORT);
+
     
+    //Serial
+    ofSetVerticalSync(true);
+    bSendSerialMessage = false;
+    ofSetLogLevel(OF_LOG_VERBOSE);
+
+    serial.listDevices();
+    vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
+
+    // this should be set to whatever com port your serial device is connected to.
+    // (ie, COM4 on a pc, /dev/tty.... on linux, /dev/tty... on a mac)
+    // arduino users check in arduino app....
+    int baud = 9600;
+//    serial.setup(0, baud); //open the first device
+    //serial.setup("COM4", baud); // windows example
+    //serial.setup("/dev/tty.usbserial-A4001JEC", baud); // mac osx example
+    //serial.setup("/dev/ttyUSB0", baud); //linux example
+    serial.setup("/dev/tty.usbmodem1451", baud);
     
+    nTimesRead = 0;
+    nBytesRead = 0;
+    readTime = 0;
+    memset(bytesReadString, 0, 4);
+    
+    unsigned char buf[4] = {'o', 'f', '!', 0x0A};
+    serial.writeBytes(&buf[0], 4);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
     listenOsc();
+    
+    //Serial
+    if (bSendSerialMessage){
+        
+        // (1) write the letter "a" to serial:
+        //serial.writeByte('a');
+//        unsigned char buf[4] = {'o', 'f', '!', 0x0A};
+//        serial.writeBytes(&buf[0], 4);
+        
+        // (2) read
+        // now we try to read 3 bytes
+        // since we might not get them all the time 3 - but sometimes 0, 6, or something else,
+        // we will try to read three bytes, as much as we can
+        // otherwise, we may have a "lag" if we don't read fast enough
+        // or just read three every time. now, we will be sure to
+        // read as much as we can in groups of three...
+        
+//        nTimesRead = 0;
+//        nBytesRead = 0;
+//        int nRead  = 0;  // a temp variable to keep count per read
+//        
+//        unsigned char bytesReturned[3];
+//        
+//        memset(bytesReadString, 0, 4);
+//        memset(bytesReturned, 0, 3);
+//        
+//        while( (nRead = serial.readBytes( bytesReturned, 3)) > 0){
+//            nTimesRead++;
+//            nBytesRead = nRead;
+//        };
+//        
+//        memcpy(bytesReadString, bytesReturned, 3);
+//        
+        bSendSerialMessage = false;
+        readTime = ofGetElapsedTimef();
+    }
     
 }
 
@@ -89,15 +151,13 @@ void ofApp::listenOsc(){
             int id_seed =  m.getArgAsInt32(0);
             char str[16];
             int str_length = ito36( id_seed, str );
-            printf("%s\n", str);
+//            printf("%s\n", str);
             
         }
-//        // check for mouse button message
-//        else if(m.getAddress() == "/mouse/button"){
-//            // the single argument is a string
-//            // mouseButtonState = m.getArgAsString(0);
-//        }
+
         
     }
         
 }
+
+
--- src/ofApp.h
+++ src/ofApp.h
@@ -13,11 +13,22 @@ class ofApp : public ofBaseApp{
 
 	public:
     
+        //OSC Receive
         ofxOscReceiver receiver;
         void listenOsc();
         int current_msg_string;
         string msg_strings[NUM_MSG_STRINGS];
         float timers[NUM_MSG_STRINGS];
+        //Serial
+        bool		bSendSerialMessage;			// a flag for sending serial
+        char		bytesRead[3];				// data from serial, we will be trying to read 3
+        char		bytesReadString[4];			// a string needs a null terminator, so we need 3 + 1 bytes
+        int			nBytesRead;					// how much did we read?
+        int			nTimesRead;					// how many times did we read?
+        float		readTime;					// when did we last read?
+        
+        ofSerial	serial;
+
     
     
 		void setup();
